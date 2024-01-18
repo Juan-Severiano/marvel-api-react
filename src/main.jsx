bedf8918b3cec36bdb558987425df908ae135ab8
Francisco Juan Severiano <juansoussev@gmail.com>
2024-01-17 22:58:08 -0300

    [add]: routes created

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './templates/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './templates/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />} path='/' />
+        <Route element={<Characters />} path='/characters' />
+        <Route element={<Home />} path='/' />
+        <Route element={<Home />} path='/' />
+        <Route element={<Home />} path='/' />
+        <Route element={<Home />} path='/' />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
