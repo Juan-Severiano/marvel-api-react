0b82c07087c11efe1d57d94da1b5a940df2555c4
Francisco Juan Severiano <juansoussev@gmail.com>
2024-01-20 16:02:54 -0300

    [finalized] character single page

--- src/services/requests/characters.js
+++ src/services/requests/characters.js
@@ -45,6 +45,51 @@ export async function getCharactersComics(id) {
   }
 }
 
+export async function getCharactersSeries(id) {
+  try {
+    const response = await api.get(`/characters/${id}/series`,);
+    return {
+      response: response,
+      results: response.data.data.results,
+    };
+  } catch (error) {
+    console.error('Erro ao obter personagens:', error);
+    return {
+      error: 'Erro ao obter personagens',
+    };
+  }
+}
+
+export async function getCharactersEvents(id) {
+  try {
+    const response = await api.get(`/characters/${id}/events`,);
+    return {
+      response: response,
+      results: response.data.data.results,
+    };
+  } catch (error) {
+    console.error('Erro ao obter personagens:', error);
+    return {
+      error: 'Erro ao obter personagens',
+    };
+  }
+}
+
+export async function getCharactersStories(id) {
+  try {
+    const response = await api.get(`/characters/${id}/stories`,);
+    return {
+      response: response,
+      results: response.data.data.results,
+    };
+  } catch (error) {
+    console.error('Erro ao obter personagens:', error);
+    return {
+      error: 'Erro ao obter personagens',
+    };
+  }
+}
+
 export async function getCharactersByNameStart(query) {
   const response = await api.get('/characters', {
     params: {
--- src/templates/Character/index.jsx
+++ src/templates/Character/index.jsx
@@ -1,29 +1,41 @@
 import { useParams } from 'react-router-dom';
 import './styles.scss';
 import { useEffect, useState } from 'react';
-import { getCharactersById, getCharactersComics } from '../../services/requests/characters';
+import { getCharactersById, getCharactersComics, getCharactersEvents, getCharactersSeries, getCharactersStories } from '../../services/requests/characters';
 import ComeBack from '../../components/ComeBack';
 import Carousel from '../../components/Carousel';
 
+const objCharacter = {
+  name: 'Loading ...',
+  title: 'Loading ...',
+  description: 'Lorem ipsim tananam',
+  thumbnail: {
+    path: '',
+    extension: ''
+  }
+}
+
 function Character() {
   const { id } = useParams()
-  const [character, setCharacter] = useState({
-    name: 'Loading ...',
-    description: 'Lorem ipsim tananam',
-    thumbnail: {
-      path: '',
-      extension: ''
-    }
-  });
-  const [comics, setComics] = useState([]);
+  const [character, setCharacter] = useState(objCharacter);
+  const [comics, setComics] = useState([objCharacter]);
+  const [series, setSeries] = useState([objCharacter]);
+  const [stories, setStories] = useState([objCharacter]);
+  const [events, setEvents] = useState([objCharacter]);
 
   useEffect(() => {
     const fetchCharacter = async () => {
       const response = await getCharactersById(id);
       const comicsResponse = await getCharactersComics(id);
-      console.log(comicsResponse);
+      const seriesResponse = await getCharactersSeries(id);
+      const eventsResponse = await getCharactersEvents(id);
+      const storiesResponse = await getCharactersStories(id);
+      console.log(storiesResponse);
       setCharacter(response.results[0]);
       setComics(comicsResponse.results);
+      setSeries(seriesResponse.results);
+      setEvents(eventsResponse.results);
+      setStories(storiesResponse.results);
     }
     fetchCharacter()
   }, [id]);
@@ -36,12 +48,13 @@ function Character() {
           <section className="content container">
             <div className="row">
               <div className="col-12 col-md-6 my-auto my-5">
-                <img src="/marvel-logo-different.jpeg" className='img-fluid w-50 mb-3' alt="" />
-                <h2 className="artist-name">{character.name}</h2>
+                <img src="/marvel-logo-different.jpeg" className='img-fluid logo' alt="" />
+                <img id='img-inside' src={`${character.thumbnail.path}.${character.thumbnail.extension}`} className='img-fluid  d-md-none mb-3' alt="" />
+                <h2 className="character-name">{character.name}</h2>
                 <p className="description" id={1}>{character.description}</p>
               </div>
-              <div className="col-12 col-md-6 p-lg-5">
-                <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} className='img-fluid' alt="" />
+              <div className="col-12 col-md-6 p-5 d-none d-md-block">
+                <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} className='img-fluid rounded rounded-3' alt="" />
               </div>
             </div>
           </section>
@@ -50,7 +63,7 @@ function Character() {
       <main className='px-auto' id='comics'>
         <article className="container px-auto">
           <section className="content container">
-            <h3 className='font-default mt-5 text-primary text-white fs-1 text-center text-md-start'>Quadrinhos com {character.name}</h3>
+            <h3 className='font-default mt-5 text-primary text-white fs-1 text-center text-md-start'>Comics with {character.name}</h3>
             <Carousel>
               {
                 comics.map(comic => (
@@ -61,6 +74,64 @@ function Character() {
                     </div>
                   </figure>
                 ))
+              }
+            </Carousel>
+          </section>
+        </article>
+      </main>
+      <main className='px-auto' id='series'>
+        <article className="container px-auto">
+          <section className="content container">
+            <h3 className='font-default mt-5 text-primary text-white fs-1 text-center text-md-start'>Series with {character.name}</h3>
+            <Carousel>
+              {
+                series.map(serie => (
+                  <figure className='serie-card item' key={serie.id}>
+                    <img src={`${serie.thumbnail.path}.${serie.thumbnail.extension}`} className='comic-img img-fluid' alt="" />
+                    <div className="comic-card-body mt-3">
+                      <h4 className='font-default text-center fs-6'>{serie.title}</h4>
+                    </div>
+                  </figure>
+                ))
+              }
+            </Carousel>
+          </section>
+        </article>
+      </main>
+      <main className='px-auto' id='events'>
+        <article className="container px-auto">
+          <section className="content container">
+            <h3 className='font-default mt-5 text-primary text-white fs-1 text-center text-md-start'>Events with {character.name}</h3>
+            <Carousel>
+              {
+                events.map(serie => (
+                  <figure className='serie-card item' key={serie.id}>
+                    <img src={`${serie.thumbnail.path}.${serie.thumbnail.extension}`} className='comic-img img-fluid' alt="" />
+                    <div className="comic-card-body mt-3">
+                      <h4 className='font-default text-center fs-6'>{serie.title}</h4>
+                    </div>
+                  </figure>
+                ))
+              }
+            </Carousel>
+          </section>
+        </article>
+      </main>
+      <main className='px-auto' id='stories'>
+        <article className="container px-auto">
+          <section className="content container">
+            <h3 className='font-default mt-5 text-primary text-white fs-1 text-center text-md-start'>Stories with {character.name}</h3>
+            <Carousel>
+              {
+                stories.length > 0 ?
+                  stories.map(story => (
+                    <figure className='stories-card item' key={story.id}>
+                      <div className="comic-card-body mt-3">
+                        <h4 className='font-default text-center fs-3'>{story.title}</h4>
+                        <p>{story.description}</p>
+                      </div>
+                    </figure>
+                  )) : 'No story related to the character was found'
               }
             </Carousel>
           </section>
